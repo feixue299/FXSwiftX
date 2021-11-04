ae336481489e927ea395ca2fd3bb8ae112e75563
feixue299 <ariablink299@gmail.com>
2021-11-04 16:38:36 +0800

    Create PropertyWrapperX.swift

---
+++ FXSwiftX/Classes/X/PropertyWrapperX.swift
@@ -0,0 +1,30 @@
+//
+//  PropertyWrapperX.swift
+//  FXSwiftX
+//
+//  Created by hard on 2021/11/4.
+//
+
+import Foundation
+
+@propertyWrapper
+public struct UserDefault<T> {
+    
+    let key: String
+    let defaultValue: T
+    
+    init(_ key: String, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+    
+    public var wrappedValue: T {
+        get {
+            return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+        }
+        set {
+            UserDefaults.standard.setValue(newValue, forKey: key)
+        }
+    }
+    
+}
