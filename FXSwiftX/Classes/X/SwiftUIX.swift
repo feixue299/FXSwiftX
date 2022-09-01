78e12f94ff4b3ec48e9a58605c2a2663e9528f9b
feixue299 <ariablink299@gmail.com>
2022-09-01 11:10:46 +0800

    Update SwiftUIX.swift

--- FXSwiftX/Classes/X/SwiftUIX.swift
+++ FXSwiftX/Classes/X/SwiftUIX.swift
@@ -71,6 +71,18 @@ public extension View {
     }
 }
 
+public extension View {
+
+  func onTapGestureForced(
+    count: Int = 1,
+    perform action: @escaping () -> Void
+  ) -> some View {
+    self
+      .contentShape(Rectangle())
+      .onTapGesture(count: count, perform: action)
+  }
+}
+
 @available(iOS 13.0, *)
 public struct SegmentView: UIViewRepresentable {
     public typealias UIViewType = SegmentControl
